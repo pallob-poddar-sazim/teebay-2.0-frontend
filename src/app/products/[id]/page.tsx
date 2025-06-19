f4cd32175c9f96a7fc156ee05e9ad514e5d1f454
pallobpoddar <pallobpoddar@gmail.com>
2025-06-19 07:35:27 +0600

    feat: create individual product details page
    This page shows details of an individual product

---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,7 @@
+import ProductDetailsLayout from "@/components/templates/ProductDetailsLayout";
+
+const ProductDetails = () => {
+  return <ProductDetailsLayout />;
+};
+
+export default ProductDetails;

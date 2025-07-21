aa031bf74bd1bd696ee269c1b06f3ffd6741802f
Pallob Poddar <pallob.poddar@sazim.io>
2025-07-21 09:21:29 +0600

    refactor: add trash icon from lucide-react

--- src/app/(userAuth)/dashboard/page.tsx
+++ src/app/(userAuth)/dashboard/page.tsx
@@ -1,8 +1,10 @@
 import ProductsBySellerContainer from "@/modules/dashboard/containers";
+import Nav from "@/shared/components/Nav";
 
 export default function Page() {
   return (
     <>
+      <Nav />
       <ProductsBySellerContainer />
     </>
   );
